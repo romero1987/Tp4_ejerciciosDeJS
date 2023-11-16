3c6c31518a2219fdbfdc71b6e8c84401806e7770
Luis Romero <1987romeroluisalberto@gmail.com>
2023-11-16 15:51:45 -0300

    ejercicio_06 sin terminar

---
+++ js/ejercicio_06.js
@@ -0,0 +1,51 @@
+/* 6- Crear una clase Libro que contenga al menos las siguientes propiedades:
+ISBN
+Título
+Autor
+Número de páginas
+
+Crear sus respectivos métodos get y set correspondientes para cada propiedad. Crear el método mostrarLibro() 
+para mostrar la información relativa al libro con el siguiente formato:
+
+“El libro xxx con ISBN xxx creado por el autor xxx tiene páginas xxx”
+
+Crear al menos 2 objetos libros y utilizar el método mostrarLibro();
+Por último, indicar cuál de los 2 objetos “libros” tiene más páginas.
+ */
+
+class Libro{
+    #ISBN;
+    constructor(ISBN,titulo, autor, numeroDePagina){
+        this.#ISBN = ISBN;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroDePagina = numeroDePagina;
+   }
+    get ISBN(){
+        return this.#ISBN;
+    }
+    set ISBN(ISBN){
+        if(nuevoISBN  <= 1000){
+            this.#ISBN = ISBN;
+        }else{
+            alert(`El numero ingresado es incorrecto`)
+        }
+    }
+
+    get titulo(){
+        return this.titulo
+    }
+    set titulo(nuevoTitulo){
+        this.titulo = nuevoTitulo;
+    }
+
+    mostrarLibros(){
+        document.write(`ISBN: ${this.ISBN}, Titulo: ${this.titulo}`);
+    }
+}
+
+const libro1 = new Libro(500, 'Martin fierro', 'Jose Hernandez', 78);
+const libro2 = new Libro(500, 'Martin fierro', 'Desconocido', 112);
+
+libro1.mostrarISBN()
+libro2.mostrarISBN()
